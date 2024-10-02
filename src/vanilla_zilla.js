35d120149b8d62e93bf8808189a663026ba88b5b
gian angelo geminiani <angelo.geminiani@gmail.com>
2024-10-02 17:49:15 +0200

    no message

--- src/vanilla_zilla.js
+++ src/vanilla_zilla.js
@@ -9,7 +9,7 @@
 !(() => {
 
     const name = "🦖 Vanilla-Zilla";
-    const v = `0.0.14`;
+    const v = `0.0.15`;
     const vPrefix = "v-"
     const vPrefixReplaceable = "v*"
     const context = (typeof window !== 'undefined') ? window : false;
